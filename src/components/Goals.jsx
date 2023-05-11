5c2fbdaf82cbd01570430b732f8b0184a6fc9268
Lizanne Cerami <lizanne.cerami@gmail.com>
2023-05-11 17:10:57 -0400

    removed broken arrows

--- src/components/Goals.jsx
+++ src/components/Goals.jsx
@@ -4,10 +4,8 @@ export default function Goals ({goal, setGoal }) {
   return(
     <Box height={100} width="100%" alignItems="center" justifyContent="center">
       <Text color="blue.300" fontSize="lg">
-        <ArrowUpIcon color="blue.100" onPress={() => setGoal(goal + 500)} /> {' '}
         <InfoIcon color="blue.300" mr={2} />
         Water Target: {goal} ml {' '}
-        <ArrowDownIcon color="blue.100" onPress={() => setGoal(goal - 500)} />
       </Text>
     </Box>
   )
